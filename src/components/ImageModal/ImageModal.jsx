034fe205fb0ddb7f4a20ddc3d0a91d2b172280e5
JoanCaba <38809439+JoanCaba@users.noreply.github.com>
2023-07-17 23:25:35 +0200

    Styles

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -3,7 +3,6 @@ const ImageModal = ({ url, imageInfo, handleModalClose }) => {
 
   return (
     <div className={'image-modal'} onClick={handleModalClose}>
-      <span className='modal-close'>&times;</span>
       <img src={url}></img>
       <footer><p>{imageInfo}</p></footer>
     </div>
