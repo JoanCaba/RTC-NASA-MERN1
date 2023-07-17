69be40cdc6195621e97805e07bd05805cffefb8e
JoanCaba <38809439+JoanCaba@users.noreply.github.com>
2023-07-18 00:09:06 +0200

    Fix logo last try

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import { msgTitle } from '/src/data/messages.json'
-// import { NASA_IMAGE_SOURCE } from '/src/data/data.json'
+import { NASA_IMAGE_SOURCE } from '/src/data/data.json'
 import './Header.css'
 import Inputs from '../Inputs/Inputs'
 const Header = ({ setSelectedApi, setSelectedDate, selectedDate, selectedApi, today }) => {
   return (
     <header>
       <div className='header-title'>
-        <img src='/src/assets/Nasa-192.png'></img>
+        <img src={NASA_IMAGE_SOURCE}></img>
         <h1>{msgTitle}</h1>
         <a href='https://api.nasa.gov/' target='_blank' rel='noopener noreferrer'>{`{NASA API}`}</a>
       </div>
