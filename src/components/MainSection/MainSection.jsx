4b093a90bd12620f2ae3921abc5781424b4a1868
JoanCaba <38809439+JoanCaba@users.noreply.github.com>
2023-07-18 13:29:50 +0200

    impl: error handling

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,19 +1,18 @@
 import Mars from '/src/components/Mars/Mars'
 import Apod from '/src/components/Apod/Apod'
 import './MainSection.css'
-import Loading from '../Loading/Loading';
 import NoData from '../NoData/NoData';
 
-const MainSection = ({ isLoading, data, selectedApi, selectedDate }) => {
-  if (!data) {
-    <main>
-      <NoData selectedDate={selectedDate} />
+const MainSection = ({ data, selectedApi, selectedDate }) => {
+  if (data.error) {
+    return <main>
+      <NoData selectedDate={selectedDate} error={data.error.message} />
     </main>
   }
 
   else return (
     <main>
-      {isLoading ? (<Loading></Loading>) : (
+      {!data ? (<NoData selectedDate={selectedDate} />) : (
 
         <>{data.photos && (
           <Mars data={data.photos} selectedDate={selectedDate}></Mars>)
--- src/components/NoData/NoData.jsx
+++ src/components/NoData/NoData.jsx
@@ -1,10 +1,18 @@
-import { msgNoData } from '/src/data/messages.json'
+import { msgNoData, msgErrorApi } from '/src/data/messages.json'
 import './NoData.css'
 
-const NoData = ({ selectedDate }) => {
+const NoData = ({ selectedDate, error }) => {
   return (
     <div className='nodata-container'>
-      <h2>{msgNoData} {selectedDate}</h2>
+
+      {error ? (
+        <>
+          <h2>{msgErrorApi}</h2>
+          <p> {error}</p>
+        </>
+      ) : (
+        <h2>{msgNoData} {selectedDate}</h2>
+      )}
       <img src="/Black-Hole.png"></img>
     </div>
   )
